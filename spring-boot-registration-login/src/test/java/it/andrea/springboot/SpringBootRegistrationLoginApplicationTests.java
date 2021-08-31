eaf66b5a6c2efa19610a2cba7cd366372cb0d05c
batterista87 <andrey87@alice.it>
2021-08-31 19:04:45 +0200

    First commit

---
+++ spring-boot-registration-login/src/test/java/it/andrea/springboot/SpringBootRegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package it.andrea.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
